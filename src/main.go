4a0fee12309df3036afcbd4db3be4069f7f0ac37
ventocilla <ventocilla.leo@gmail.com>
2024-12-11 10:55:36 -0300

    Versão 4.0 - 11/12/24

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo - 3.5")
+	fmt.Fprintf(w, "Aplicacao exemplo - 4.0 - 11/12/24")
 }
 
 func main() {
